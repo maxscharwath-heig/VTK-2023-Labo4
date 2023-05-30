f65bfda06117cc33c31b87e525f91587937b2fa2
Maxime Scharwath <maxscharwath@gmail.com>
2023-05-30 12:56:41 +0200

    mesh

--- main.py
+++ main.py
@@ -3,41 +3,22 @@ import vtkmodules.vtkInteractionStyle
 import vtkmodules.vtkRenderingOpenGL2
 from vtkmodules.vtkCommonColor import vtkNamedColors
 from vtkmodules.vtkRenderingCore import (
-    vtkRenderWindow,
     vtkRenderWindowInteractor,
 )
-from vtkmodules.vtkRenderingCore import vtkLight
-
-import vtkmodules.vtkCommonDataModel as vtkCommonDataModel
-import numpy as np
 
 colors = vtkNamedColors()
 
 
+def read_structured_grid(filename):
+    reader = vtk.vtkStructuredGridReader()
+    reader.SetFileName(filename)
+    reader.Update()
+    return reader.GetOutput()
+
+
 def main():
-    # Read DEM data using numpy
-    dem_filename = "EarthEnv-DEM90_N60E010.bil"
-    dem_data = np.fromfile(dem_filename, dtype=np.int16)
-    dem_data = dem_data.reshape((6000, 6000)).astype(float)
-
-    # Create a structured grid from the DEM data
-    grid = vtkCommonDataModel.vtkStructuredGrid()
-    grid.SetDimensions(6000, 6000, 1)
-
-    vtk_points = vtk.vtkPoints()
-    vtk_scalars = vtk.vtkFloatArray()
-
-    # Assign the DEM data to the scalars array
-    for x in range(6000):
-        for y in range(6000):
-            vtk_points.InsertNextPoint(x, y, dem_data[x, y])
-            vtk_scalars.InsertNextValue(dem_data[x, y])
-
-    vtk_structured_grid = vtk.vtkStructuredGrid()
-    vtk_structured_grid.SetDimensions(6000, 6000, 1)
-    vtk_structured_grid.SetPoints(vtk_points)
-    vtk_structured_grid.GetPointData().SetScalars(vtk_scalars)
-    vtk_structured_grid.Modified()
+    # Read the structured grid
+    vtk_structured_grid = read_structured_grid('DEM90.vtk')
 
     mapper = vtk.vtkDataSetMapper()
     mapper.SetInputData(vtk_structured_grid)
@@ -45,19 +26,23 @@ def main():
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
 
+    # Create a texture map
+    texture_map = vtk.vtkTexture()
+    image_reader = vtk.vtkJPEGReader()
+    image_reader.SetFileName("glider_map.jpg")
+    image_reader.Update()
+    texture_map.SetInputDataObject(image_reader.GetOutput())
+
+    # Apply the texture map to the actor
+    actor.SetTexture(texture_map)
+
     # Set up the renderer
-    renderer = vtkmodules.vtkRenderingCore.vtkRenderer()
+    renderer = vtk.vtkRenderer()
     renderer.AddActor(actor)
     renderer.SetBackground(colors.GetColor3d("White"))
 
-    # Add a light source to the scene
-    light = vtkLight()
-    light.SetLightTypeToSceneLight()
-    light.SetPosition(1, 1, 1)
-    renderer.AddLight(light)
-
     # Set up the render window
-    render_window = vtkRenderWindow()
+    render_window = vtk.vtkRenderWindow()
     render_window.AddRenderer(renderer)
     render_window.SetSize(1200, 1200)
     render_window.SetWindowName('VTK - Labo 4')
--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from pyproj import CRS, Transformer
 
 # Initialize coordinate transformation
-crs_rt90 = CRS("EPSG:2400")  # RT90
+crs_rt90 = CRS("EPSG:3021")  # RT90
 crs_wgs84 = CRS("EPSG:4326")  # WGS84
 transformer = Transformer.from_crs(crs_rt90, crs_wgs84)
 
@@ -26,3 +26,26 @@ with open('vtkgps.txt', 'r') as file:
 # Print the converted data
 for record in converted_data[:5]:
     print(record)
+
+# Print the converted data ( of the given image )
+# Haut-gauche (8.672884552833715, 60.79103974072171)
+# Haut-droite (8.814207271697969, 60.815339449115214)
+# Bas-droite (8.832192925809254, 60.709118845984214)
+# Bas-gauche (8.690603591508864, 60.68481654100474)
+coords_rt90 = [
+    (1349340, 7022573),  # Haut-gauche (8.672884552833715, 60.79103974072171)
+    (1371573, 7022967),  # Haut-droite (8.814207271697969, 60.815339449115214)
+    (1371835, 7006362),  # Bas-droite (8.832192925809254, 60.709118845984214)
+    (1349602, 7005969)   # Bas-gauche (8.690603591508864, 60.68481654100474)
+]
+
+converted_coords = []
+
+for coord in coords_rt90:
+    # Convert coordinates from RT90 to WGS84
+    lon, lat = transformer.transform(coord[0], coord[1])
+    converted_coords.append((lon, lat))
+
+# Print the converted coordinates
+for coord in converted_coords:
+    print(coord)
